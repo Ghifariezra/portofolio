b621a9fd0b6dfcd4afe426b50d038a97967d849e
Owner <ghifariezraramadhan@gmail.com>
2025-09-23 03:12:37 +0700

    UPDATE : METADATA

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import type { MetadataRoute } from 'next'
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const client = new PortfolioService()
     const projects = await client.getProjects()
-
+ 
     return [
         {
             url: "https://www.ezdev.xyz/",
