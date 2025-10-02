c4817121257a2a4fd7b9d9775cd778845c4c54dc
Owner <ghifariezraramadhan@gmail.com>
2025-10-02 20:17:51 +0700

    update: remove tiktok link

--- src/utilities/socials/socials.ts
+++ src/utilities/socials/socials.ts
@@ -4,5 +4,5 @@ export const SocialItems: MenuItemType[] = [
     { name: "GitHub", href: "https://github.com/Ghifariezra" },
     { name: "LinkedIn", href: "https://www.linkedin.com/in/ghifariezraramadhan/" },
     { name: "Medium", href: "https://medium.com/@ghifariezraramadhan" },
-    { name: "Tiktok", href: "https://www.tiktok.com/@ezdev7" },
+    // { name: "Tiktok", href: "https://www.tiktok.com/@ezdev7" },
 ];
