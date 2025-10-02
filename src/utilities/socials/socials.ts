05c79040d5fd64323cbe4902c0bf85f49b5fb910
Owner <ghifariezraramadhan@gmail.com>
2025-10-02 20:13:42 +0700

    update: tiktok link

--- src/utilities/socials/socials.ts
+++ src/utilities/socials/socials.ts
@@ -4,4 +4,5 @@ export const SocialItems: MenuItemType[] = [
     { name: "GitHub", href: "https://github.com/Ghifariezra" },
     { name: "LinkedIn", href: "https://www.linkedin.com/in/ghifariezraramadhan/" },
     { name: "Medium", href: "https://medium.com/@ghifariezraramadhan" },
+    { name: "Tiktok", href: "https://www.tiktok.com/@ezdev7" },
 ];
