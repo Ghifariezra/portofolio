1223d82a086480e67ccd08a9606195bb41819ac4
Owner <ghifariezraramadhan@gmail.com>
2025-11-15 19:03:27 +0700

    update: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,14 @@ export default async function middleware(request: NextRequest) {
     const authToken = (await cookies()).get("authToken")?.value || "";
     const { pathname, origin } = request.nextUrl;
 
-    // Kalau user sudah login → blokir akses ke /auth/login
     if (authToken && pathname.startsWith('/auth/login')) {
         return NextResponse.redirect(new URL('/dashboard', origin));
     }
 
-    // Kalau user belum login → blokir akses ke /dashboard
     if (!authToken && pathname.startsWith('/dashboard')) {
         return NextResponse.redirect(new URL('/auth/login', origin));
     }
 
-    // Lanjutkan request normal
     return NextResponse.next();
 }
 
