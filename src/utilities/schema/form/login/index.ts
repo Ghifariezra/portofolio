d253876d1ae461e12b7a710236078f099600a7f5
Owner <ghifariezraramadhan@gmail.com>
2025-11-15 19:33:46 +0700

    update: schema login

--- src/utilities/schema/form/login/index.ts
+++ src/utilities/schema/form/login/index.ts
@@ -1,10 +1,19 @@
 import { z } from "zod";
+import validator from "validator";
 
 export const schemaFormLogin = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-    password: z.string().min(2, {
-        message: "Password must be at least 2 characters.",
+    username: z.string().min(8, {
+        message: "Username must be at least 8 characters.",
+    }).transform((username) => validator.escape(username)),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters.",
+    }).refine((password) => validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    }), {
+        message: "Password must be strong.",
     }),
 });
